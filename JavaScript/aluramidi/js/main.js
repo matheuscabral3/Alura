09b01c75b66ebfd401ba5660eb620e2ca520f692
Matheus M <https://github.com/matheuscabral3/Alura.git>
2024-05-15 11:37:49 -0300

    Adicionado máscara e validações para o AluraFone

--- JavaScript/aluramidi/js/main.js
+++ JavaScript/aluramidi/js/main.js
@@ -1,5 +1,33 @@
-let botao = document.querySelector('button');
+$(document).ready(function () {
+    // JAVASCRIPT PURO 
+    // var botao = document.querySelector('button');
+    // botao.addEventListener('click', function(e) {
+    //     e.preventDefault();
+    //     alert('Fui clicado!');
+    // });
+    // console.log('Arquivo carregado!');
 
-botao.addEventListener('click', function() {
-    alert('Fui clicado!');
+    let numeroClicado = '';
+    // jQuery
+    $(".telefoneDDD").inputmask("(99) 99999-9999");
+    // $(".telefoneSemDDD").inputmask("9999-9999");
+
+    $("input:button").click(function (e) {
+        // Ir adicionando conforme for clicando nos números.
+        numeroClicado += e.target.defaultValue;
+
+        if (numeroClicado.length < 12) {
+            $("#txtTelefoneDDD").inputmask("(99) 99999-9999")[0].value = numeroClicado;
+            // $("#txtTelefone2").inputmask("9999-9999")[0].value = numeroClicado;
+        } else {
+            alert('Excedeu os números de caracteres');
+        }
+    });
+
+    $("#limparNumero").click(function () {
+        $("#txtTelefoneDDD").inputmask("(99) 99999-9999")[0].value = "";
+    });
+
 });
+
+
