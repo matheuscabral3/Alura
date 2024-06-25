4c9e201310d133f6cb1e749e03f2d23b0852eee2
Matheus M <https://github.com/matheuscabral3/Alura.git>
2024-06-25 12:42:41 -0300

    Repositório recriado para ajustes

--- AluraAPI/FilmesApi/FilmesApi/Controllers/WeatherForecastController.cs
+++ AluraAPI/FilmesApi/FilmesApi/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace AluraBooks.Controllers
+namespace FilmesApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
@@ -23,7 +23,7 @@ namespace AluraBooks.Controllers
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
--- AluraAPI/FilmesApi/FilmesApi/WeatherForecast.cs
+++ AluraAPI/FilmesApi/FilmesApi/WeatherForecast.cs
@@ -1,8 +1,8 @@
-namespace AluraBooks
+namespace FilmesApi
 {
     public class WeatherForecast
     {
-        public DateOnly Date { get; set; }
+        public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
 
