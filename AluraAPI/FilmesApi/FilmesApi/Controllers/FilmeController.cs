8a31c90fdc051747247ca45f29a0b9439bc78955
Matheus M <https://github.com/matheuscabral3/Alura.git>
2024-06-26 17:05:28 -0300

    Aula 3 - Finalizado: Listagem, paginação, Busca por ID

--- AluraAPI/FilmesApi/FilmesApi/Controllers/FilmeController.cs
+++ AluraAPI/FilmesApi/FilmesApi/Controllers/FilmeController.cs
@@ -8,18 +8,41 @@ namespace FilmesApi.Controllers;
 public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
 
     [HttpPost]
-    public void AdicionaFilme([FromBody] Filme filme)
+    public IActionResult AdicionaFilme([FromBody] Filme filme)
     {
-
+        filme.Id = id++;
         filmes.Add(filme);
 
-        Console.WriteLine(filme.Titulo);
-        Console.WriteLine(filme.Genero);
-        Console.WriteLine(filme.Diretor);
-        Console.WriteLine(filme.Duracao);
+        // Após ser feita a criação do Filme, a RESPOSTA de retorno será exibido o filme que acabou de ser inserido.
+        return CreatedAtAction(nameof(RecuperaFilmePorId), new { id = filme.Id }, filme);
 
+    }
+
+
+    // LISTAR TODOS
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 10)
+    {
+        // Obter os números a partir da URL de Requisição. (HttpGet)
+        // Exemplo:
+        // https://localhost:7140/filme?skip=10&take=5
+        // Retorna a lista a partir do index (0), pegando apenas 5 registros.
+
+        return filmes.Skip(skip).Take(take);
+    }
+
+
+    // LISTAR APENAS O FILME COM 'ID' INFORMADO
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmePorId(int id)
+    {
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+
+        if (filme == null) return NotFound();
+        return Ok(filme);
     }
 
 }
--- AluraAPI/FilmesApi/FilmesApi/Models/Filme.cs
+++ AluraAPI/FilmesApi/FilmesApi/Models/Filme.cs
@@ -4,6 +4,9 @@ namespace FilmesApi.Models;
 
 public class Filme
 {
+    public int Id { get; set; }
+
+
     [Required(ErrorMessage = "O Título do filme é obrigatório !")]
     [StringLength(30)]
     public string Titulo { get; set; }
@@ -23,5 +26,4 @@ public class Filme
     [Range(1, 180, ErrorMessage = "A duração deve ter entre 1 e 180 minutos.")]
     public int Duracao { get; set; }
 
-    
 }
